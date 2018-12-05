7ed759e4ff95f77eba932210a6dc43ec0a5e3cdf
FlorianBesel <maliarts2@gmail.com>
2018-12-05 12:28:04 +0100

    Keks

--- src/Model/World.java
+++ src/Model/World.java
@@ -10,7 +10,6 @@ public class World extends GameObject {
 
     public World(int x, int y, int width, int height, String filePath){
         super(x, y, width, height, filePath);
-        x = array[x]
     }
 //Array[x][y] 32px*32px
     @Override
