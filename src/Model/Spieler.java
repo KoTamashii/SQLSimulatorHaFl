eabc2b89846a94a4fb1061363da94eb3b1f75cb1
FlorianBesel <maliarts2@gmail.com>
2018-12-07 11:15:40 +0100

    Keks

--- src/Model/Spieler.java
+++ src/Model/Spieler.java
@@ -74,4 +74,59 @@ public class Spieler extends GameObject implements InputManager {
     public void setGeld(int geld) {
         this.geld = geld;
     }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
 }
